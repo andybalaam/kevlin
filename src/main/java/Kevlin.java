577d14c561938b92959629c6be8307d655e32afe
Andy Balaam <andybalaam@artificialworlds.net>
2016-08-19 17:27:23 +0100

    Appender class to make initial tests pass

--- src/main/java/Kevlin.java
+++ src/main/java/Kevlin.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Kevlin
 {
     // TODO:
@@ -14,29 +17,142 @@ public class Kevlin
         tests();
     }
 
+    private static void a(AstNode node, String expected)
+    {
+        String actual = format(node);
+        if (!actual.equals(expected))
+        {
+            System.err.println("Assertion failed.  Actual then expected:");
+            System.err.println(actual.replaceAll("\n", "\\\\n"));
+            System.err.println(expected.replaceAll("\n", "\\\\n"));
+            System.exit(1);
+        }
+    }
+
     private static void tests()
     {
-        assert format(
-            list(
-                list(n("class"), n("Foo")),
-                block("{", "}", list())
-            )
-        ).equals("class Foo\n{\n}\n");
+        System.out.println("Running tests ...");
+        Tests.Short_variable_declaration_on_one_line();
+        Tests.Class_block_wraps_even_if_fits();
+        System.out.println("All tests passed.");
+    }
+
+    private static class Tests
+    {
+        private static void Short_variable_declaration_on_one_line()
+        {
+            a(
+                list(list(p("int"), p("x")), p("="), list(p("3"), n(";"))),
+                "int x = 3;"
+            );
+        }
+
+        private static void Class_block_wraps_even_if_fits()
+        {
+            a(
+                list(
+                    list(p("class"), p("Foo")),
+                    block("{", "}", list())
+                ),
+                "class Foo\n{\n}\n"
+            );
+        }
+    }
+
+    private static class Appender
+    {
+        private final StringBuilder str = new StringBuilder();
+        private boolean pad = false;
+
+        public void add(AstNode node)
+        {
+            if (pad && node.pad)
+            {
+                str.append(" ");
+            }
+            if (node.contents != null)
+            {
+                str.append(node.contents);
+                pad = node.pad;
+            }
+            if (node.start != null)
+            {
+                str.append("\n");
+                str.append(node.start);
+                str.append("\n");
+            }
+            for (AstNode child : node.children)
+            {
+                add(child);
+            }
+            if (node.end != null)
+            {
+                str.append(node.end);
+                str.append("\n");
+            }
+        }
+
+        public String formatted()
+        {
+            return str.toString();
+        }
     }
 
     private static String format(AstNode node)
     {
-        return "";
+        Appender ret = new Appender();
+        ret.add(node);
+        return ret.formatted();
     }
 
     private static class AstNode
     {
         public final String contents;
+        public final boolean pad;
+        public final String start;
+        public final String end;
+        public final AstNode[] children;
+
+        public AstNode(String contents, boolean pad)
+        {
+            this.contents = contents;
+            this.pad = pad;
+            this.start = null;
+            this.end = null;
+            this.children = new AstNode[0];
+        }
 
         public AstNode(String contents)
         {
             this.contents = contents;
+            this.pad = false;
+            this.start = null;
+            this.end = null;
+            this.children = new AstNode[0];
         }
+
+        public AstNode(String start, String end, AstNode... children)
+        {
+            this.contents = null;
+            this.pad = false;
+            this.start = start;
+            this.end = end;
+            this.children = children;
+        }
+
+        public AstNode(AstNode... children)
+        {
+            this.contents = null;
+            this.pad = false;
+            this.start = null;
+            this.end = null;
+            this.children = children;
+        }
+    }
+
+    private static AstNode p(String contents)
+    {
+        return new AstNode(contents, true);
     }
 
     private static AstNode n(String contents)
@@ -46,11 +162,11 @@ public class Kevlin
 
     private static AstNode list(AstNode... items)
     {
-        return new AstNode("bar");
+        return new AstNode(items);
     }
 
     private static AstNode block(String start, String end, AstNode... items)
     {
-        return new AstNode("foo");
+        return new AstNode(start, end, items);
     }
 }
